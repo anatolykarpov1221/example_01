aced46e3abad8ae94e20d5e124f72351cd68e087
Anatoly Karpov <karpov1221@gmail.com>
2023-11-27 11:35:46 +0100

    fix git is easy

--- src/test/Test.java
+++ src/test/Test.java
@@ -4,6 +4,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("===============");
         System.out.println("---------------");
-        System.out.println("Git is easy");
+        System.out.println("Git as is easy");
     }
 }
